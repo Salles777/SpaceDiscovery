294cf77c654f183582fb60719af8f77f8d533a13
Cristiano Salles <cristianosalles2011@gmail.com>
2023-11-21 21:13:41 -0300

    Atualização onde é gerado os pontos no mapa onde foi escolhido para demarcar a estrela, também aparecendo o menu de opções do que fazer dentro do Space Marker, mas não ainda funcional

--- main.py
+++ main.py
@@ -17,13 +17,17 @@ def inicializated():
 inicializated()
 
 pygame.init()
+black = (0, 0, 0)
+white = (255, 255, 255)
 size = (1000,516)
 clock = pygame.time.Clock()
 icon = pygame.image.load("space.png")
 pygame.display.set_icon(icon)
-screan = pygame.display.set_mode(size)
+scream = pygame.display.set_mode(size)
 background = pygame.image.load("bg.jpg")
 pygame.display.set_caption("Space Marker")
+pygame.mixer.music.load("Space_Machine_Power.mp3")
+pygame.mixer.music.play(-1)
 running = True
 while running:
     for event in pygame.event.get():
@@ -39,8 +43,22 @@ while running:
             arquivo = open("star_list.txt","w")
             arquivo.write(json.dumps(star_list))
             arquivo.close()
-
-    screan.blit(background,(0,0))
+     
+    scream.blit(background,(0,0))
+    font = pygame.font.Font("freesansbold.ttf", 15)
+    text = font.render("Pressione F11 para Carregar pontos", True, white)
+    scream.blit(text, (20,30))
+    text2 = font.render("Pressione F12 para Deletar os pontos", True, white)
+    scream.blit(text2, (20,50))
+    text3 = font.render("Após colocar o ponto o salvamento é automático", True, white)
+    scream.blit(text3, (20,70))
+    arquivo = open("star_list.txt", "r")
+    try:
+        star_list = json.loads(arquivo.read())
+    except:
+        star_list = {}
+    for nome, pos in star_list.items():
+        pygame.draw.circle(scream, white, pos, 7)        
 
     pygame.display.update()
     clock.tick(60)
