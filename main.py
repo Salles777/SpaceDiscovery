7eab209963740d8555e61d94c99f9181805f4b3d
Cristiano Salles <cristianosalles2011@gmail.com>
2023-11-21 21:37:50 -0300

    Atualizacao feita com os pontos sendo ligadas por linhas e escrevendo o nome que na anterior ainda nao estavam aparecendo

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ pygame.display.set_caption("Space Marker")
 pygame.mixer.music.load("Space_Machine_Power.mp3")
 pygame.mixer.music.play(-1)
 running = True
+first = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -43,22 +44,29 @@ while running:
             arquivo = open("star_list.txt","w")
             arquivo.write(json.dumps(star_list))
             arquivo.close()
+        #elif event.type == pygame.
      
     scream.blit(background,(0,0))
     font = pygame.font.Font("freesansbold.ttf", 15)
-    text = font.render("Pressione F11 para Carregar pontos", True, white)
-    scream.blit(text, (20,30))
     text2 = font.render("Pressione F12 para Deletar os pontos", True, white)
-    scream.blit(text2, (20,50))
+    scream.blit(text2, (20,30))
     text3 = font.render("Após colocar o ponto o salvamento é automático", True, white)
-    scream.blit(text3, (20,70))
+    scream.blit(text3, (20,50))
     arquivo = open("star_list.txt", "r")
     try:
         star_list = json.loads(arquivo.read())
     except:
         star_list = {}
     for nome, pos in star_list.items():
-        pygame.draw.circle(scream, white, pos, 7)        
+        pygame.draw.circle(scream, white, pos, 7)
+        star_name = font.render(nome, True, white)
+        scream.blit(star_name,(pos[0]+10,pos[1]+10))
+        if first == True:
+            back = pos
+            first = False
+        else:
+            pygame.draw.line(scream, white, back, pos, 2)
+            back = pos
 
     pygame.display.update()
     clock.tick(60)
