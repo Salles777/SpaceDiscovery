639a2e48ed8566f1e0f1b95cd88bc262e6842882
Cristiano Salles <cristianosalles2011@gmail.com>
2023-11-24 16:04:42 -0300

    Foi arrumado o cód. da parte de criação do executável

--- gera_setup.py
+++ gera_setup.py
@@ -6,8 +6,8 @@ cx_Freeze.setup(
     name = "Space Marker",
     options = {
         "build_exe":{
-            "packages":["pygame", "json", ""],
-            "include_files":["bg.jpg", "star_list", "space.png", "Space_Machine_Power.mp3"]
+            "packages":["pygame", "tkinter", "ast", "math"],
+            "include_files":["bg.jpg", "SalvarPontos.txt", "space.png", "Space_Machine_Power.mp3"]
         }
     }, executables = executables
 )
